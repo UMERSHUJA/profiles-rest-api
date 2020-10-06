7e0aeaef10e9c8f84c6b60af967e03c2f3b480a3
UMERSHUJA <umershuja12@gmail.com>
2020-10-06 20:13:04 +0500

    APIView

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
 
 from profiles_api.models import UserProfile
+
 admin.site.register(UserProfile)
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """ Test API View """
+    def get(self, request, format=None):
+        """ Return a list of APIView features """
+
+        an_apiview = [
+        'Uses http methods as function(get, post, update, put, patch, delete)',
+        'its similiar to traditional django view',
+        'give you the most control over your application logic',
+        'Is mapped manually to urls',
+        ]
+
+        return Response(
+            {
+                'message': 'Hello!',
+                'an_apiview': an_apiview
+            }
+        )
